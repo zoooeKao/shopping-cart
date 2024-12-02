1c734ed9fea39ab69fcc1cda3bca1e2da61fb787
zoooeKao <kaoyw0619@gmail.com>
2024-12-02 14:33:32 +0800

    chore: advise vite.config target

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:4000',
+        // target: 'http://127.0.0.1:4000',
+        target: 'https://dummy-shopping-cart.hnd1.zeabur.app/',
         changeOrigin: true,
       },
     },
